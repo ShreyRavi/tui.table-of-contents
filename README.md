03508a34310f9460ecd1dbfd2be02290145f93b2
Shreyas Tallamraju <shreyastallamraju@gmail.com>
2021-03-07 19:32:06 -0600

    initial commit of npm package format

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
 # [tui.tableofcontents](https://shreyravi.github.io/tui.tableofcontents/)
-A table of contents custom plugin for Toast UI. Render a table of contents in real-time! 
-To test, clone repo and run `npm install && npm start`.
-<p align="center"><img src="https://github.com/ShreyRavi/tui.tableofcontents/raw/main/screenshot.png" height="93%" width="93%"></img>Screenshot of the demo of tui.tableofcontents Toast-UI Plugin.</p>
 
-## How to Run
+[![npm (scoped)](https://img.shields.io/npm/v/@shreyravi/tui-table-of-contents.svg)](https://www.npmjs.com/package/@shreyravi/tui-table-of-contents)
+[![npm bundle size (minified)](https://img.shields.io/bundlephobia/min/@shreyravi/tui-table-of-contents.svg)](https://www.npmjs.com/package/@shreyravi/tui-table-of-contents)
 
-1. Clone repository (`gh repo clone ShreyRavi/tui.tableofcontents` with Github CLI)
+A table of contents custom plugin for Toast UI. Render a table of contents in real-time.
 
-2. Run `npm install` inside repository directory
+<p align="center"><img src="https://github.com/ShreyRavi/tui-editor-example/raw/main/screenshot.png" height="93%" width="93%"></img>Screenshot of the demo of tui.tableofcontents Toast-UI Plugin.</p>
 
-3. Run `npm start` inside repository directory
+A package to help decrypt Caesar shift cipher-encrypted strings with an unknown shift value.
 
-4. Navigate to `http://localhost:3000/` (usually)
+## Install
+```
+$ npm install @shreyravi/tui.table-of-contents
+```
 
-## Instructions
-
-Use the 'open book'/Insert Table of Contents button on the toolbar to insert a table of contents automatically at the beginning of the document.
-
-## WIP Disclaimer
-
-The repository is meant to be a showcase and is not a final, finished product. Data is pre-filled to simulate real-world use cases and certain functionality is not completed.
-
-## File Structure
-### Primary Files
-- `src/EditorExample.js` - contains main React for example
-- `src/tuiTableOfContents.js` - contains ToastUI plugin code
-### Other Files
-- `screenshot.png` - a screenshot of the plugin in action
-- `README.md` - self-explanatory
-- `LICENSE` - MIT License
+## Usage
+```
+TBD
+```
 
 ## License
-MIT License. Open Source.
+MIT License. Open Source. Contact [author](mailto:shrey@shreyravi.com) with any concerns.
--- src/index.js
+++ src/index.js
@@ -1,16 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { searchHeadings } from './search';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function stripHtmlTags(md) {
+  let doc = new DOMParser().parseFromString(md, 'text/html');
+  return doc.body.textContent || "";
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function groupByLevel(elements, result=[]) {
+  if (!elements.length) {
+    return [];
+  }
+  const levelResult = [elements[0]];
+  let i = 0;
+  while ((i + 1) < elements.length) {
+    if (elements[i].level === elements[i + 1].level) {
+      levelResult.push(elements[i + 1]);
+      i += 1;
+    } else {
+      break;
+    }
+  }
+  return result.concat([levelResult], groupByLevel(elements.slice(levelResult.length), result));
+}
+
+
+function generateList(elements, tocOptions) {
+  const { title, bulletType } = tocOptions;
+  return `${`<ol type="${bulletType}">`.repeat(elements[0].level - 1)}${elements.filter(element => element.name !== title).map(element => `<li>${element.name}</li>`).join('')}${"</ol>".repeat(elements[0].level - 1)}`;
+}
+
+/**
+ * renderTableOfContents
+ * @param {*} tocWrapperId The wrapper ID to edit 
+ * @param {*} md A String version of markdown to parse
+ * @param {{title: String,  bulletType: String}} tocOptions An object of options for toc render, with title being the title of the Table of Contents, bulletType being type of bullet shown (eg. 1 = number, I = Roman numerals, i = lowercase Roman numerals)
+ * @param {String} tocInput Raw input from codeBlock (not used currently)
+ */
+function renderTableOfContents(md, tocOptions) {
+  return `<h${tocOptions.headingSize}> ${tocOptions.title}</h${tocOptions.headingSize}><ol type="${tocOptions.bulletType}">${groupByLevel(searchHeadings(stripHtmlTags(md))).map(headings => generateList(headings, tocOptions)).join('')}</ol>`;
+}
+
+function insertTableOfContents(editor, md,pluginOptions) {
+  const el = document.querySelector(`#toc`);
+  if (!el) { // if first time called, insert div
+    editor.setMarkdown(`<span id="toc"></span>\n${editor.getMarkdown()}`);
+  } else {
+    el.parentElement.removeChild(el);
+  }
+  updateTableOfContents(md, pluginOptions);
+}
+
+function updateTableOfContents(md, pluginOptions) {
+  const el = document.querySelector(`#toc`);
+  if (!el) { // if not already inserted
+    return;  // do nothing
+  }
+  el.innerHTML = `${renderTableOfContents(md, pluginOptions)}`;
+}
+
+function initUI(editor, pluginOptions) {
+  editor.getUI().getToolbar().insertItem(-1, {
+    type: 'button',
+    options: {
+        event: 'insertTableOfContents',
+        tooltip: 'Toggle Table of Contents',
+        text: '📖',
+        style: 'color: black; background: white;'
+    }
+  });
+  editor.eventManager.addEventType('insertTableOfContents');
+  editor.eventManager.listen('insertTableOfContents', () => insertTableOfContents(editor, editor.getMarkdown(), pluginOptions));
+  editor.eventManager.listen('change', () => updateTableOfContents(editor.getMarkdown(), pluginOptions))
+}
+
+/**
+ * tuiTableOfContents
+ * @param {Editor|Viewer} editor Instance of Editor or Viewer from Toast UI
+ * @param {{title: String,  headingSize: Number, bulletType: String}} pluginOptions An object of options for toc render, with title being the title of the Table of Contents, headingSize being the size of title, bulletType being type of bullet shown (eg. 1 = number, I = Roman numerals, i = lowercase Roman numerals) (WIP, not fully functional)
+ */
+export default function tableOfContentsPlugin(editor, pluginOptions={title: "Table of Contents", headingSize: 1, bulletType: "1"}) {
+  const { codeBlockManager } = Object.getPrototypeOf(editor).constructor;
+  codeBlockManager.setReplacer('toc', (tocInput) => {
+    return `<span id="toc"></span>`;
+  });
+  initUI(editor, pluginOptions);
+}
